ae79f87a05a97c862ea57a58f75fe8375119969d
PEPS-TECH1007 <testingpeps1007@gmail.com>
2026-09-15 11:49:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,22 +2,31 @@ import streamlit as st
 from ultralytics import YOLO
 from PIL import Image
 import numpy as np
+from huggingface_hub import hf_hub_download
 
-# Load trained model
-model = YOLO(
-    r"C:\Charan\Egg defect detection\runs\detect\egg_detection\yolo11x_kaggle_v6_100ep\weights\best.pt"
+
+# Download trained model from Hugging Face
+model_path = hf_hub_download(
+    repo_id="PepsTechRnD/egg-quality-yolo11x",
+    filename="best.pt"
 )
+
+# Load trained YOLO11x model
+model = YOLO(model_path)
+
 
 # Page title
 st.title("🥚 Egg Quality Detection")
 
 st.write("Upload an egg image to test the trained YOLO11x model.")
 
+
 # Upload image
 uploaded_file = st.file_uploader(
     "Upload an egg image",
     type=["jpg", "jpeg", "png"]
 )
+
 
 # If image is uploaded
 if uploaded_file is not None:
@@ -41,4 +50,7 @@ if uploaded_file is not None:
         result_image = results[0].plot()
 
         # Display result
-        st.image(result_image, caption="Detection Result")
+        st.image(
+            result_image,
+            caption="Detection Result"
+        )
