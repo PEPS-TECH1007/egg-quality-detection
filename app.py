eab5f7a71b44819679e625f7a26f2feb5a4578c1
PEPS-TECH1007 <testingpeps1007@gmail.com>
2026-09-15 11:46:36 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,44 @@
+import streamlit as st
+from ultralytics import YOLO
+from PIL import Image
+import numpy as np
+
+# Load trained model
+model = YOLO(
+    r"C:\Charan\Egg defect detection\runs\detect\egg_detection\yolo11x_kaggle_v6_100ep\weights\best.pt"
+)
+
+# Page title
+st.title("🥚 Egg Quality Detection")
+
+st.write("Upload an egg image to test the trained YOLO11x model.")
+
+# Upload image
+uploaded_file = st.file_uploader(
+    "Upload an egg image",
+    type=["jpg", "jpeg", "png"]
+)
+
+# If image is uploaded
+if uploaded_file is not None:
+
+    # Read uploaded image
+    image = Image.open(uploaded_file).convert("RGB")
+
+    # Display uploaded image
+    st.image(image, caption="Uploaded Image")
+
+    # Detection button
+    if st.button("🔍 Detect Egg"):
+
+        # Convert PIL image to NumPy array
+        image_array = np.array(image)
+
+        # Run YOLO
+        results = model(image_array)
+
+        # Draw detections
+        result_image = results[0].plot()
+
+        # Display result
+        st.image(result_image, caption="Detection Result")
